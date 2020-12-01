ec99e0b66846c9476a1b07e814ea497d6dd59b65
Artemis21 <hollyshort21@gmail.com>
2020-12-01 05:31:52 +0000

    Add day 1 solution.

--- src/day_01.py
+++ src/day_01.py
@@ -1,18 +1,27 @@
+import itertools
+
 import aoc_helper
 
-raw = aoc_helper.day(1)
-print(raw)
 
-def parse_raw():
-    ...
+def parse_raw(raw):
+    return list(map(int, raw.split('\n')))
 
-data = parse_raw()
 
 def part_one():
-    ...
+    a, b = next(filter(
+        lambda x: sum(x) == 2020, itertools.combinations(data, 2)
+    ))
+    return a * b
+
 
 def part_two():
-    ...
+    a, b, c = next(filter(
+        lambda x: sum(x) == 2020, itertools.combinations(data, 3)
+    ))
+    return a * b * c
+
+
+data = parse_raw(aoc_helper.day(1))
 
 aoc_helper.submit(day=1, solv_func=part_one)
 aoc_helper.submit(day=1, solv_func=part_two)
