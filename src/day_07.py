ede00476346eb01e98c09b77f1d59ecde94875e1
Artemis21 <hollyshort21@gmail.com>
2020-12-08 08:00:39 +0000

    Days 7-8

--- src/day_07.py
+++ src/day_07.py
@@ -1,18 +1,44 @@
 import aoc_helper
 
 raw = aoc_helper.day(7)
-print(raw)
 
 def parse_raw():
-    ...
+    bags = {}
+    for line in raw.split('\n'):
+        container, inside = line.split('s contain ')
+        inside_bags = {}
+        for inside_part in inside[:-1].split(', '):
+            if inside_part == 'no other bags':
+                break
+            number = int(inside_part[0])
+            bag = inside_part[2:].rstrip('s')
+            inside_bags[bag] = number
+        bags[container] = inside_bags
+    return bags
 
 data = parse_raw()
 
 def part_one():
-    ...
+    can_carry = ['shiny gold bag']
+    old_can_carry = None
+    while old_can_carry != can_carry:
+        old_can_carry = list(can_carry)
+        for bag in data:
+            if bag in can_carry:
+                continue
+            for can_carry_bag in can_carry:
+                if can_carry_bag in data[bag]:
+                    can_carry.append(bag)
+                    break
+    print(can_carry)
+    return len(set(can_carry)) - 1
 
-def part_two():
-    ...
+
+def part_two(container_bag='shiny gold bag'):
+    total = 1
+    for bag, amount in data[container_bag].items():
+        total += part_two(bag) * amount
+    return total
 
 aoc_helper.submit(day=7, solv_func=part_one)
-aoc_helper.submit(day=7, solv_func=part_two)
+aoc_helper.submit(day=7, solv_func=lambda: part_two() - 1)
--- src/day_08.py
+++ src/day_08.py
@@ -1,18 +1,62 @@
 import aoc_helper
 
-raw = aoc_helper.day(8)
-print(raw)
 
 def parse_raw():
-    ...
+    ops = []
+    for line in raw.split('\n'):
+        op, raw_num = line.split()
+        ops.append((op, int(raw_num)))
+    return ops
 
-data = parse_raw()
 
 def part_one():
-    ...
+    already_executed = []
+    index = 0
+    acc = 0
+    while index not in already_executed:
+        print(index, len(data))
+        op, arg = data[index]
+        already_executed.append(index)
+        if op == 'nop':
+            index += 1
+        elif op == 'acc':
+            index += 1
+            acc += arg
+        elif op == 'jmp':
+            index += arg
+    return acc
+
 
 def part_two():
-    ...
+    for change_index, op_and_arg in enumerate(data):
+        if op_and_arg[0] == 'nop':
+            program = list(data)
+            program[change_index] = ('jmp', op_and_arg[1])
+        elif op_and_arg[0] == 'jmp':
+            program = list(data)
+            program[change_index] = ('nop', op_and_arg[1])
+        else:
+            continue
+        print(change_index)
+        already_executed = []
+        index = 0
+        acc = 0
+        while index not in already_executed:
+            try:
+                op, arg = program[index]
+            except IndexError:
+                return acc
+            already_executed.append(index)
+            if op == 'nop':
+                index += 1
+            elif op == 'acc':
+                index += 1
+                acc += arg
+            elif op == 'jmp':
+                index += arg
 
+
+raw = aoc_helper.day(8)
+data = parse_raw()
 aoc_helper.submit(day=8, solv_func=part_one)
 aoc_helper.submit(day=8, solv_func=part_two)
