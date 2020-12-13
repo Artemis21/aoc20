a882d6d85a7d6c4e8620ec33ba732b6e43d60e37
Artemis21 <hollyshort21@gmail.com>
2020-12-13 07:37:09 +0000

    Day 13. Don't care about mess anymore lol.

--- src/day_12.py
+++ src/day_12.py
@@ -1,18 +1,66 @@
+import math
+
 import aoc_helper
 
 raw = aoc_helper.day(12)
-print(raw)
 
 def parse_raw():
-    ...
+    moves = []
+    for line in raw.split('\n'):
+        moves.append((line[0], int(line[1:])))
+    return moves
 
 data = parse_raw()
 
 def part_one():
-    ...
+    x = y = angle = 0
+    for action, value in data:
+        if action == 'N':
+            y += value
+        elif action == 'E':
+            x += value
+        elif action == 'S':
+            y -= value
+        elif action == 'W':
+            x -= value
+        elif action == 'L':
+            angle -= value
+        elif action == 'R':
+            angle += value
+        elif action == 'F':
+            x += value * math.cos(math.radians(angle))
+            y -= value * math.sin(math.radians(angle))
+    return round(abs(x) + abs(y))
 
 def part_two():
-    ...
+    x_ship = y_ship = 0
+    x_waypoint = 10
+    y_waypoint = 1
+    for action, value in data:
+        if action == 'N':
+            y_waypoint += value
+        elif action == 'E':
+            x_waypoint += value
+        elif action == 'S':
+            y_waypoint -= value
+        elif action == 'W':
+            x_waypoint -= value
+        elif action == 'L':
+            angle = math.atan2(y_waypoint, x_waypoint)
+            angle += math.radians(value)
+            distance = (x_waypoint ** 2 + y_waypoint ** 2) ** 0.5
+            x_waypoint = distance * math.cos(angle)
+            y_waypoint = distance * math.sin(angle)
+        elif action == 'R':
+            angle = math.atan2(y_waypoint, x_waypoint)
+            angle -= math.radians(value)
+            distance = (x_waypoint ** 2 + y_waypoint ** 2) ** 0.5
+            x_waypoint = distance * math.cos(angle)
+            y_waypoint = distance * math.sin(angle)
+        elif action == 'F':
+            x_ship += x_waypoint * value
+            y_ship += y_waypoint * value
+    return round(abs(x_ship) + abs(y_ship))
 
-aoc_helper.submit(day=12, solv_func=part_one)
+# aoc_helper.submit(day=12, solv_func=part_one)
 aoc_helper.submit(day=12, solv_func=part_two)
--- src/day_13.py
+++ src/day_13.py
@@ -4,15 +4,35 @@ raw = aoc_helper.day(13)
 print(raw)
 
 def parse_raw():
-    ...
+    raw_mins, raw_buses = raw.split('\n')
+    buses = []
+    for raw_bus in raw_buses.split(','):
+        if raw_bus == 'x':
+            buses.append(None)
+        else:
+            buses.append(int(raw_bus))
+    return int(raw_mins), buses
 
 data = parse_raw()
 
 def part_one():
-    ...
+    min_time, buses = data
+    soonest_bus = min((bus for bus in buses if bus), key=lambda bus: bus - (min_time % bus))
+    print(soonest_bus, min_time, min_time % soonest_bus)
+    return (soonest_bus - (min_time % soonest_bus)) * soonest_bus
 
 def part_two():
-    ...
+    _, buses = data
+    buses = list(sorted((i for i in enumerate(buses) if i[1]), key=lambda x: -x[1]))
+    first, step = buses[0]
+    first = (step - first) % step
+    for idx, bus in buses[1:]:
+        value = first
+        while value % bus != (bus - idx) % bus:
+            value += step
+        first = value
+        step *= bus
+    return '{:f}'.format(first).split('.')[0]
 
 aoc_helper.submit(day=13, solv_func=part_one)
 aoc_helper.submit(day=13, solv_func=part_two)
